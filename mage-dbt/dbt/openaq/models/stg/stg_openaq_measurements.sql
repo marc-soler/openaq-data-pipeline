ec56a2a3511bf55abb0f47267330dbeb8cd01793
Marc Soler <93591805+marc-soler@users.noreply.github.com>
2024-04-13 17:50:53 +0200

    dbt models stg and core

--- mage-dbt/dbt/openaq/models/stg/stg_openaq_measurements.sql
+++ mage-dbt/dbt/openaq/models/stg/stg_openaq_measurements.sql
@@ -1,6 +1,10 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 
--- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+with measurements_data as (
+    select * from {{ source('') }}
+)
